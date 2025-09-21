b3cf035f03598fecd5d7e3a0d1c56bb0e026c2b5
Liam Dalziel <liamdzl100@gmail.com>
2025-09-21 15:36:03 +0100

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -1,23 +1,14 @@
 from math import comb
 import numpy as np
 
-
-# ----- DYNAMIC PROGRAMMING, ALGORITHMS 101 -----
-
 def compositions(d, q):
     if d == 1:
-        return [[q]]                                   # necessary base case, function is recursive
-    result = []                                        # we're aiming to produce a VECTOR of VECTORS, all combinations that satisfy constraints. We initialise an empty array
+        return [[q]]                            
+    result = []                                     
     for i in range(q + 1):
-        for tail in compositions(d - 1, q - i):        # start running thru previous LISTS (not just prev.), since they satisfied q - i constraint they'll satisfy this one... 
-            result.append([i] + tail)                  # we start with [0, ...]. now this actually needs same q since i = 0, but DIMENSION was done before. So can reuse
+        for tail in compositions(d - 1, q - i):       
+            result.append([i] + tail)                
     return result                                      
-
-# ----- SUMMARY -----
-# ----- think of initialise for loop as asking 'OK, start ALL with vectors of the form [i,....]. 
-# We need all the subvectors of 1dim less that sum to q... wait shit ..since we already have a leading i, 
-# only need to sum up to q - i !'
-
 
 def forms(d,p):
     result = []
